8857c0719de0f5c8228d403c7e0ae2a58eeaf9af
jmacneill66 <jeffrey.macneill@gmail.com>
2025-02-28 11:00:05 -0400

    updated main.py

--- src/main.py
+++ src/main.py
@@ -62,9 +62,17 @@ def generate_page(from_path, template_path, dest_path, basepath):
     html_content = markdown_to_html_node(markdown_content).to_html()
     title = extract_title(markdown_content)
 
+    # Replace template placeholders
     output_content = template_content.replace(
         "{{ Title }}", title).replace("{{ Content }}", html_content)
     output_content = output_content.replace('{{ Basepath }}', basepath)
+
+    # Use regular expressions to replace any href or src that starts with /
+    import re
+    output_content = re.sub(
+        r'href="(/[^"]*)"', f'href="{basepath}\\1"', output_content)
+    output_content = re.sub(
+        r'src="(/[^"]*)"', f'src="{basepath}\\1"', output_content)
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
